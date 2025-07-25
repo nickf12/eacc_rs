1b7686a6a3de0fafa20ea04d9999bb5ce303e80e
Nickf12 <nicco.magnoni@gmail.com>
2025-07-25 16:04:30 +0200

    Add test for uploading image to X

--- src/bin/test_x_post.rs
+++ src/bin/test_x_post.rs
@@ -1,6 +1,12 @@
+use reqwest::Body;
 use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use twitter_api_v1::endpoints::media::upload_media::upload_image;
 use twitter_api_v1::endpoints::tweets::manage_tweets::create_tweet;
 use twitter_api_v1::endpoints::EndpointRet;
+use twitter_api_v1::objects::MediaCategory;
 use twitter_api_v1::TokenSecrets;
 
 #[tokio::main]
@@ -24,14 +30,59 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .danger_accept_invalid_certs(true)
         .build()?;
 
+    // Path to the image file
+    let image_path = "./media/hoodCart.jpeg";
+
+    let mut file = File::open(image_path).map_err(|e| format!("Can't find image, error: {e}"))?;
+
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)
+        .expect("Failed to read image file");
+
+    // Get the file size for stream_length
+    let file_size = file
+        .metadata()
+        .map_err(|e| format!("Error checking the file size: {e}"))?
+        .len();
+
+    let media_category = MediaCategory::TweetImage;
+    let stream = Body::from(buffer);
+    let file_name = Some(
+        Path::new(image_path)
+            .file_name()
+            .unwrap()
+            .to_string_lossy()
+            .into_owned(),
+    );
+    // Upload the image
+    let upload_result = upload_image(
+        &token_secrets,
+        client.clone(), // Clone the client for reuse
+        media_category,
+        stream,
+        Some(file_size),
+        file_name,
+    )
+    .await?;
+
+    // Extract media_id from the response
+    let media_id = match upload_result {
+        EndpointRet::Ok(response_body) => response_body.media_id, // Assuming UploadResponseBodyOkJson has a media_id field
+        _ => panic!(
+            "Failed to upload image, unexpected response: {:?}",
+            upload_result
+        ),
+    };
+
     let ret = create_tweet(
         &token_secrets,
         client,
         Some("Hello world, this is my first post on X from a Rust application!"),
-        None,
+        Some(vec![media_id]),
         None,
     )
     .await?;
+
     match ret {
         EndpointRet::Ok(ok_json) => {
             println!("create_tweet:{ok_json:?}");
